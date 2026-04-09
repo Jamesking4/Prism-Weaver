9ca0e48078e455760d7de54499b2f0d000420563
Shutov Zahar <zahar07shutov@gmail.com>
2026-04-09 22:38:12 +0500

    Добавлен конструктор LightSource

--- PrismWeaver/Content/LightSourse.cs
+++ PrismWeaver/Content/LightSourse.cs
@@ -1,6 +1,52 @@
-﻿namespace PrismWeaver.Content;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
-public class LightSourse
+namespace PrismWeaver.Content;
+
+public class LightSource : GameObject
 {
+    private Direction direction;
+    private bool isWork = true;
+    private readonly Texture2D texture;
+    private Light light;
+    private int lightWidth = 1000;
+    private int lightHeight = 5;
+    private Color color;
     
+    public LightSource(Vector2 startPosition, Texture2D texture, Direction direction, 
+        Color color, int width, int height, Texture2D textureTemp) : base(startPosition, width, height)
+    {
+        this.texture = textureTemp;
+        this.direction = direction;
+        this.color = color;
+        
+        light = new Light(GetStartLight(), lightWidth, lightHeight, color, isWork, direction, texture);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+        
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(texture, Rectangle, color);
+    }
+
+    public Light GetLight()
+    {
+        return light;
+    }
+
+    public void SetIsWork(bool isWork)
+    {
+        this.isWork = isWork;
+    }
+
+    public Vector2 GetStartLight()
+    {
+        var posX = direction == Direction.Left ? -Rectangle.Width / 2  : Rectangle.Width / 2;
+        var posY = direction == Direction.Up ? -Rectangle.Height / 2 : Rectangle.Height / 2;
+        return new Vector2(posX + Rectangle.X, posY + Rectangle.Y - lightHeight / 2);
+    }
 }
