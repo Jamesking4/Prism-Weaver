e923534ff6b6acc9be00f85361e2b32ce694850c
Shutov Zahar <zahar07shutov@gmail.com>
2026-04-09 22:38:02 +0500

    Добавлен конструктор Light

--- PrismWeaver/Content/Light.cs
+++ PrismWeaver/Content/Light.cs
@@ -1,6 +1,34 @@
-﻿namespace PrismWeaver.Content;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
-public class Light
+namespace PrismWeaver.Content;
+
+public class Light : GameObject
 {
+    private Color color;
+    private Direction direction;
+    private Texture2D texture;
+    private bool enabled;
     
+    public Light(Vector2 startPosition, int width, int height, Color color, bool isWork, Direction direction, Texture2D texture) 
+        : base(startPosition, width, height)
+    {
+        this.color = color;
+        this.direction = direction;
+        enabled = isWork;
+        
+        this.texture = texture;
+        texture.SetData([color]);
+    }
+
+    public override void Update(GameTime gameTime)
+    {
+    }
+
+    public override void Draw(SpriteBatch spriteBatch)
+    {
+        if (enabled)
+            spriteBatch.Draw(texture, Rectangle, color);
+    }
 }
